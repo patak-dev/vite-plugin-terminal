b7670e3d9088ec5b568600ca406d2da3308b11bd
buu <ibushuai@gmail.com>
2022-01-21 11:32:27 +0800

    fix: add missing types (#3)

--- client.d.ts
+++ client.d.ts
@@ -1,8 +1,12 @@
 declare module 'virtual:terminal' {
+  export type Message = string | Record<string, unknown>
+  
   export const terminal: {
-    log: (message: string) => void
-    warn: (message: string) => void
-    error: (message: string) => void
+    log: (message: Message) => void
+    warn: (message: Message) => void
+    error: (message: Message) => void
+    info: (message: Message) => void
+    assert: (assertion: boolean, message: Message) => void
   }
   export default terminal
 }
